2a177c485dd098abae6538349e54ecc4c045814a
Tim Jedrek <tim@timjedrek.com>
2022-07-02 22:34:18 -0500

    configuracion para devise en rails por user modelo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :fiesta
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # DEVISE USER RAILS 7 STUFF #
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # FROM SCAFFOLDING FIESTA #
+  resources :fiesta
 end
