83458e249bdd4f88db54105b0f9528142e0c80bc
Tim Jedrek <tim@timjedrek.com>
2022-07-03 22:01:15 -0500

    try to fix db but not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :created_fiestum, foreign_key: :creator_id, class_name: 'Fiestum', dependent: :destroy
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :created_fiestum, foreign_key: :creator_id, class_name: 'Fiestum', dependent: :destroy
 end
--- db/migrate/20220704021935_add_index_to_users.rb
+++ db/migrate/20220704021935_add_index_to_users.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_index :users, :username
+    add_index :users, :username, unique: true
   end
 end
