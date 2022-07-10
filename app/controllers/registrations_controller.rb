ec400a3275a8ed44a4a750dd63aaeab4307e0db4
Tim Jedrek <tim@timjedrek.com>
2022-07-10 06:56:37 -0500

    elimina comento porque registration controlador es necesario y cuando crear nueva perfil, username no hay

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,3 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
   end 
 end
-
-### I commented out the reference to this controller in the routes file.  Not sure why this was here in the first place.
-### The intention I beleive for this file is to allow sign-in using a username in addition to the email. 
-### But the ruby erb file that has the form has the field set as an email field so it checks for validity.
