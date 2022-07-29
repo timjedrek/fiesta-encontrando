d7db3a62752851d0a770ba79fcd86c734875ec12
Tim Jedrek <tim@timjedrek.com>
2022-07-29 18:06:31 -0500

    anchor no trabajando

--- app/controllers/attending_fiesta_controller.rb
+++ app/controllers/attending_fiesta_controller.rb
@@ -8,13 +8,14 @@ class AttendingFiestaController < ApplicationController
     @fiestum = Fiestum.find(params[:id])
     @fiestum.attendees << current_user
     flash[:notice] = "Se registró para la fiesta. Genial!"
-    redirect_to request.referer, anchor: "fiestum_#{@fiestum.id}" # Anchor isn't working....
+    redirect_to request.referer #, anchor:  "fiestum_#{@fiestum.id}"
   end
 
   def destroy
     @fiestum = Fiestum.find(params[:id])
     @fiestum.attendees.delete(current_user)
     flash[:notice] = "¿Por qué no quieres ir a la fiesta? No registrado."
-    redirect_to root_path
+    #redirect_back(fallback_location: root_path)   #<--- this aint working
+    redirect_to root_path #, anchor: "fiestum_#{@fiestum.id}"
   end
 end
